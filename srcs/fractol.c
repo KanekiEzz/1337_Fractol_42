6007da11e89a3f02353e68aa11d547b5e9e6e92a
Ilyass Ezzam <iezzam@e1r10p12.1337.ma>
2024-12-06 10:31:32 +0100

    l3almya

---
+++ srcs/fractol.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iezzam <iezzam@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/06 09:32:50 by iezzam            #+#    #+#             */
+/*   Updated: 2024/12/06 10:30:29 by iezzam           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/fractol.h"
+
+void parse_fractal()
+{
+}
+
+int main(int ac, char **av)
+{
+	// t_complex	acav;
+	(void)ac;
+	(void)av;
+
+	if (ac <= 1)
+		write(1, "add this ./fractol julia or ./fractol mandelbrot\n", 49);
+	else if (ac == 2)
+	{
+		if (strcmp(av[1], "mandelbrot") == 0)
+		{
+			write(1, "see image mandelbrot\n", 21);
+		}
+		else
+			return write(1, "add this ./fractol julia <number> <number> or ./fractol mandelbrot\n", 49);
+	}
+	else if (ac == 4)
+	{
+		if 
+	}
+	else
+		exit();
+	// parse_fractal(&acav, av[1]);
+	return 0;
+}
